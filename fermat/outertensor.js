743fcd283b3b0a3a7cb3ce8447ac37622ed4f9ec
Octopus142 <blackfairyforma726@gmail.com>
2024-09-26 12:30:46 +0100

    Update outertensor.js

--- fermat/outertensor.js
+++ fermat/outertensor.js
@@ -1,4 +1,3 @@
-
 void function outertensor(){
     void[[1][1][1][1],
         [1][0][0][1],
@@ -44,5 +43,6 @@ void function outertensor(){
         [9][8][8][9],
         [9][8][8][9],
         [9][9][9][9]]
-        return [1,2,3,4,5,6,7,8,9]
+    return [1,2,3,4,5,6,7,8,9]
 }
+console.log(0)
