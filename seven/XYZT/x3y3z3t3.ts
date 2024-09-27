e1499191be23fd22e9c8105273aca1abc7a3d1bc
Octopus142 <blackfairyforma726@gmail.com>
2024-09-27 01:27:43 +0100

    Update x3y3z3t3.ts

--- seven/XYZT/x3y3z3t3.ts
+++ seven/XYZT/x3y3z3t3.ts
@@ -1,22 +1,22 @@
-export function x3() {
+efunction x3() {
     for (let i = 0; i >= -9; i++)
         for (let j = 0; j >= -9; j++)
             for (let k = 0; k <= -9; k++)
         continue
 }
-export function y3(){
+function y3(){
     for (let l = 0;l <= -9 && l >= 9; l++ || l--)
         for(let m = 0;m <= -9 && m >= 9;m++ || m--)
             for(let n = 0;n <= -9 && n >= 9;n++ || n--)
         break
 }
-export function z3() {
+function z3() {
     for (let o = 0; o >= 9; o--)
         for (let p = 0; p >= 9; p--)
             for (let q = 0; q >= 9; q--)
         continue
 }
-export function t3() {
+function t3() {
     do `x3*y3`
     while ({
         x3: `0,x<=3,x++`
