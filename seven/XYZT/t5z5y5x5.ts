fc12c74d37659dfa1e5ba7e356874492db71c415
Octopus142 <blackfairyforma726@gmail.com>
2024-09-27 01:28:42 +0100

    Update t5z5y5x5.ts

--- seven/XYZT/t5z5y5x5.ts
+++ seven/XYZT/t5z5y5x5.ts
@@ -1,24 +1,24 @@
 
-export function x5(){
+function x5(){
     for (let I = 0; I <= 4; ++I)
        for (let J= 0;J <= 4; ++J)
           for (let K= 0; K <= 4;++K)
         break
 }
-export function y5() {
+function y5() {
     for (let L = 0; L <= -4 && L <= 4; ++L || L--)
         for (let M = 0; M <= -4 && M <= 4; ++M || M--)
             for (let N = 0; N <= -4 && N <= 4; ++N || N--)
         continue
 }
 
-export function z5(){
+function z5(){
     for (let O= 0; O >= 4; --O)
        for (let P = 0; P >= 4; --P)
           for (let Q = 0; Q >= 4; --Q)
         break
 }
-export function t5(){
+function t5(){
     do `x5*y5`
     while ({
         z5: `0,z>=5,z--`
